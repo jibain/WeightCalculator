40c7a024593d254479b3071d00b0beceab1b9a62
jibain <ianbain96@gmail.com>
2024-01-18 13:47:37 -0600

    Testing commit

--- WCmain/src/Main.java
+++ WCmain/src/Main.java
@@ -2,5 +2,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Weight Calculator");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the weight of your bar: ");
+        String input = s.nextLine().toLowerCase();
+
+
     }
 }
